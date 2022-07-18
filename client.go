6f164d6098c200e790c81803822c8da6579b3f6a
JohnII7 <157798378@qq.com>
2022-07-18 22:42:26 +0800

    IM-客户端-命令行解析

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -30,8 +31,19 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+// ./client -ip xxx.x.x.x
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器Ip(默认127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认8888)")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	// 命令行解析
+	flag.Parsed()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>连接服务器失败")
 		return
